5b8e753b23533feed69910739428f95a3685c5df
pigicoder <pigi.gariboldi@gmail.com>
2022-12-12 00:23:37 +0100

    Update homeAcquirente.php
    aggiunti button search e order

--- frontend/homeAcquirente.php
+++ frontend/homeAcquirente.php
@@ -20,6 +20,12 @@ $restaurants=getRestaurantsByZone ($cid,$zona);
 			<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         		<div class="container">
                 		<a class="navbar-brand" href="../index.html">Home</a>
+						<form class="d-flex" role="search">
+        					<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        					<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+							<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Order</button>
+      					</form>
+
             		</div>
         	</nav>
 	    	<div class="background">
