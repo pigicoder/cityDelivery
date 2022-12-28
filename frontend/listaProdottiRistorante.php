fe2e08b9af9fcc7e83f9127e5df283fa083be11d
yost <yost@bullShenkerEye.homenet.telecomitalia.it>
2022-12-28 03:18:47 +0100

    added cart

--- frontend/listaProdottiRistorante.php
+++ frontend/listaProdottiRistorante.php
@@ -1,7 +1,9 @@
+<!DOCTYPE html>
 <?php
 
-require "../common/db_connection.php";
-require "../backend/db_config.php";
+/*require "../common/db_connection.php";
+require "../backend/db_config.php";*/
+require "../common/functions.php";
 
 $result = dbConnection();
 $email_ristorante=$_GET["email_ristorante"];
@@ -16,8 +18,9 @@ $result = $cid->query("SELECT nome, tipo, descrizione, prezzo, immagine FROM Pro
 <html>
 	<head>
 		<title>Products</title>
-        <link rel="icon" type="image/x-icon" href="assets/waiter.ico" />
+        <link rel="icon" type="image/x-icon" href="../assets/waiter.ico" />
 		<link rel="stylesheet" href="../css/styles.css">
+        <script src="../js/scripts.js" async></script>
 	</head>
 	<body>
 	    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -30,11 +33,10 @@ $result = $cid->query("SELECT nome, tipo, descrizione, prezzo, immagine FROM Pro
                 <button onclick="location.href='basket.php'" class="btn btn-outline-success my-2 my-sm-0" type="submit">Order</button>
     		</div>
         </nav>
-		<div class="background container">
+		<div class="background">
             <h1><?=  $r_sociale; ?> products:</h1> 
-            <div class="row">
+            <section class="row">
             <?php
-            $ordered = ["restaurant","product","quantity"];
 		    while($row = $result->fetch_row())
 		    {
 		    ?>
@@ -44,17 +46,17 @@ $result = $cid->query("SELECT nome, tipo, descrizione, prezzo, immagine FROM Pro
                             <div class="col-md-8">
                                 <div class="card-body">
                                     <h5 class="card-title"><?=$row[0]?></h5>
-                                    <p class="card-text"><?=$row[2]?></p>
-                                    <button onclick="showHide('<?php echo $row[0]?>_form')" name="add" class="btn btn-primary">Aggiungi all'ordine</button>
-                                    <form name="addQty" id="<?php echo $row[0]?>_form" style="visibility: hidden" action="../backend/insertProductIntoBasket.php">
-                                        <div>Insert quantity: </div>
-                                        <input type="number"/>
-                                        <input type="submit" value = "add"/>
+                                    <p class="card-text" style="text-align: left"><?=$row[2]?></p>
+                                    <p class="card-price">€<?=$row[3]?></p>
+                                    <button onclick="showHide('<?php echo $row[0]?>_form')" class="btn btn-primary">Add to cart</button>
+                                    <form id="<?php echo $row[0]?>_form" style="visibility: hidden;">Insert quantity: 
+                                        <input class="card-quantity-input" type="number" name="quantity" value="1" />
+                                        <button onclick="showHide('<?php echo $row[0]?>_form')" class="btn btn-primary add-to-cart" type="button">Add</button>
                                     </form>
                                 </div>
                             </div>
                             <div class="col-md-4">
-                                <img src="data:image/jpg;base64,<?=base64_encode($row[4])?>" class=card-img-top alt="..."></img>
+                                <img src="data:image/jpg;base64,<?=base64_encode($row[4])?>" class="card-img-top" alt="..."></img>
                             </div>
                         </div>
                     </div>
@@ -62,46 +64,23 @@ $result = $cid->query("SELECT nome, tipo, descrizione, prezzo, immagine FROM Pro
             <?php
 		    }
 	        ?>
-            </div>
+            </section>
+            <section class="row-2">
+                <h2 class="section-header">CART</h2>
+                <div class="cart">
+                    <div class="cart-header">
+                        <span class="cart-item cart-header cart-column">PRODUCT</span>
+                        <span class="cart-price cart-header cart-column">PRICE</span>
+                        <span class="cart-quantity cart-header cart-column">QUANTITY</span>
+                    </div>    
+                    <div class="cart-items"></div>
+                    <div class="cart-total">
+                        <strong class="cart-total-title">Total</strong>
+                        <span class="cart-total-price">€0</span>
+                    </div>
+                    <button class="btn btn-primary btn-purchase" type="button">PURCHASE</button>
+                </div>
+            </section>
         </div>
 	</body>
-    <script>
-        function showHide(id)
-        {
-            var x = document.getElementById(id);
-            if (x.style.visibility === "hidden")
-                {
-                    x.style.visibility = "visible";
-                }
-                else
-                {
-                    x.style.visibility = "hidden";
-                }
-        }
-
-
-
-        /*const addButton = document.getElementsByName("add");
-        const form = document.getElementsByName("addQty");
-        console.log();
-
-        for (var i=0; i<form.length; i++)
-        {
-            console.log(form[i].style.visibility);
-            addButton.forEach(function(addBtn)
-            {
-                addBtn.addEventListener("click", () =>
-                {
-                    if (form[i].style.visibility === "hidden")
-                    {
-                        form[i].style.visibility = "visible";
-                    }
-                    else
-                    {
-                        form[i].style.visibility = "hidden";
-                    }
-                });
-            });
-        }*/
-    </script>
 </html>
--- js/scripts.js
+++ js/scripts.js
@@ -7,61 +7,201 @@
 // Use this file to add JavaScript to your project
 
 // ---FORM_STYLES SCRIPTS--- //
+/*
 $(document).ready(function(){
-        
-        //-- Click on terms and conditions
-        $(".term").click(function(){
-            var ctrl = $(this).find("i");
-            if (ctrl.hasClass("fa-check-square-o")){
-                ctrl.attr("class","fa fa-square-o");
-            }else{
-                ctrl.attr("class", "fa fa-check-square-o");
-            }
-        }) 
 
-        $("input").blur(function(){
-            if ($(this).val() != ""){
-                $(this).parent().css({"color":"black"});
-                $(this).css({"border-bottom":"1px solid silver","color":"gray"});                 
-            }
-        })
-        
-        //--- CONTINUE ---
-        $("form > p > a").click(function(){
-            //-- Detect terms and conditions
-            var term = false;
-            if ($(".term > i").hasClass('fa-check-square-o')){
-                term = true;
-            }
-            
-            //-- only example
-            var user = {};
-            user.name = $("input[name='name']").val();
-            user.id = $("input[name='id']").val();
-            user.phone = $("input[name='phone']").val();
-            user.email = $("input[name='email']").val();
-            user.term = term;
+    //-- Click on terms and conditions
+    $(".term").click(function(){
+        var ctrl = $(this).find("i");
+        if (ctrl.hasClass("fa-check-square-o")){
+            ctrl.attr("class","fa fa-square-o");
+        }else{
+            ctrl.attr("class", "fa fa-check-square-o");
+        }
+    }) 
 
-            //-- Validator            
-            $("input").each(function(e, valor){
-                var error = false;
-                if ($(this).val() == ""){
-                    error = true;
-                }
-                if (error === true){
-                    //-- with errors
-                    $(this).parent().css({"color":"red"});
-                    $(this).css({"border-bottom":"1px solid red"});
-                }else{
-                    //-- without errors
-                    $(this).parent().css({"color":"black"});
-                    $(this).css({"border-bottom":"1px solid silver","color":"gray"}); 
-                }
-            })
-
-            //-- msg example
-            $("body").append(JSON.stringify(user) + "<br>");
-        })
+    $("input").blur(function(){
+        if ($(this).val() != ""){
+            $(this).parent().css({"color":"black"});
+            $(this).css({"border-bottom":"1px solid silver","color":"gray"});                 
+        }
     })
+    
+    //--- CONTINUE ---
+    $("form > p > a").click(function(){
+        //-- Detect terms and conditions
+        var term = false;
+        if ($(".term > i").hasClass('fa-check-square-o')){
+            term = true;
+        }
+        
+        //-- only example
+        var user = {};
+        user.name = $("input[name='name']").val();
+        user.id = $("input[name='id']").val();
+        user.phone = $("input[name='phone']").val();
+        user.email = $("input[name='email']").val();
+        user.term = term;
 
-    // ---END FORM_STYLE SCRIPTS--- //
+        //-- Validator            
+        $("input").each(function(e, valor){
+            var error = false;
+            if ($(this).val() == ""){
+                error = true;
+            }
+            if (error === true){
+                //-- with errors
+                $(this).parent().css({"color":"red"});
+                $(this).css({"border-bottom":"1px solid red"});
+            }else{
+                //-- without errors
+                $(this).parent().css({"color":"black"});
+                $(this).css({"border-bottom":"1px solid silver","color":"gray"}); 
+            }
+        })
+
+        //-- msg example
+        $("body").append(JSON.stringify(user) + "<br>");
+    })
+})
+*/
+// ---END FORM_STYLE SCRIPTS--- //
+
+// ---START CART SCRIPTS--- //
+
+function showHide(id)
+{
+    var x = document.getElementById(id)
+    if (x.style.visibility === "hidden")
+    {
+        x.style.visibility = "visible"
+    }
+    else
+    {
+        x.style.visibility = "hidden"
+    }
+}
+
+if (document.readyState == 'loading')
+{
+    document.addEventListener('DOMContentLoaded', ready)
+}
+else
+{
+    ready()
+}
+
+function ready()
+{
+    var removeCartItemButtons = document.getElementsByClassName('btn-remove')
+    for (var i = 0; i < removeCartItemButtons.length; i++)
+    {
+        var button = removeCartItemButtons[i]
+        button.addEventListener('click', removeCartItem)
+    }
+
+    var quantityInputs = document.getElementsByClassName('cart-quantity-input')
+    for (var i = 0; i < quantityInputs.length; i++)
+    {
+        var input = quantityInputs[i]
+        input.addEventListener('change', quantityChanged)
+    }
+
+    var addToCartButtons = document.getElementsByClassName('add-to-cart')
+    for (var i = 0; i < addToCartButtons.length; i++)
+    {
+        var button = addToCartButtons[i]
+        button.addEventListener('click', addToCartClicked)
+    }
+
+    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+}
+
+function purchaseClicked()
+{
+    alert('Thank you for your order')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    while (cartItems.hasChildNodes())
+    {
+        cartItems.removeChild(cartItems.firstChild)
+    }
+    updateCartTotal()
+}
+
+function removeCartItem(event)
+{
+    var buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.remove()
+    updateCartTotal()
+}
+
+function quantityChanged(event)
+{
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0)
+    {
+        input.value = 1
+    }
+    updateCartTotal()
+}
+
+function addToCartClicked(event)
+{
+    var button = event.target
+    var shopItem = button.parentElement.parentElement.parentElement
+    var title = shopItem.getElementsByClassName('card-title')[0].innerText
+    var price = shopItem.getElementsByClassName('card-price')[0].innerHTML.replace('€', '')
+    var quantity = shopItem.getElementsByClassName('card-quantity-input')[0].value
+    console.log(quantity)
+    addItemToCart(title, price, quantity)
+    updateCartTotal()
+}
+
+function addItemToCart(title, price, quantity)
+{
+    var cartRow = document.createElement('div')
+    cartRow.classList.add('cart-row')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+    for (var i = 0; i < cartItemNames.length; i++)
+    {
+        if (cartItemNames[i].innerText == title)
+        {
+            alert('This item is already added to the cart')
+            return
+        }
+    }
+    var cartRowContents = `
+        <div class="cart-item cart-column">
+            <span class="cart-item-title">${title}</span>
+        </div>
+        <span class="cart-price-el cart-column">€${price}</span>
+        <div class="cart-quantity cart-column">
+            <input class="cart-quantity-input" type="number" value="${quantity}">
+            <button class="btn-remove" type="button">REMOVE</button>
+        </div>`
+    cartRow.innerHTML = cartRowContents
+    cartItems.append(cartRow)
+    cartRow.getElementsByClassName('btn-remove')[0].addEventListener('click', removeCartItem)
+    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
+}
+
+function updateCartTotal()
+{
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0
+    for (var i = 0; i < cartRows.length; i++)
+    {
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        var price = parseFloat(priceElement.innerText.replace('€', ''))
+        var quantity = quantityElement.value
+        total = total + (price * quantity)
+    }
+    total = Math.round(total * 100) / 100
+    document.getElementsByClassName('cart-total-price')[0].innerText = '€' + total
+}
+
+// ---END CART SCRIPTS--- //
